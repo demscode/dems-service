3d87f614512aacd2dc5f2b8f116dcea73046f492
Lochlan Bunn <loklaan@gmail.com>
2014-08-13 13:28:04 +1000

    Added configs

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    wiredep: {
+      target: {
+        src: 'public/views/index.html'
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'server.js', 'routes/*', 'public/js/*'],
+      options: {
+        // None
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default tasks.
+  grunt.registerTask('setup', ['wiredep']);
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', []);
+
+};
