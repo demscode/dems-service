9987b7af52dbd635de9973735537fc00f3a156e7
Kristian Matzen <kristian_motzen@hotmail.com>
2014-08-27 10:41:19 +1000

    Patient model and test added
    Patient model added with id, token, name and email
    
    Model and test is based on carer model and test

---
+++ models/patient.js
@@ -0,0 +1,27 @@
+/**
+ * Patient Schema and Model
+ * Functionality for the Patient persistence.
+ */
+
+(function (exports) {
+  'use strict';
+
+  /**
+   * Initialises model with given db settings.
+   * @param  {object} settings  MongoDB adapter settings
+   * @return {object}           JugglingDB model object
+   */
+  exports.init = function(settings) {
+    var Schema = require('jugglingdb').Schema;
+
+    var schemaMongo = new Schema('mongodb', settings);
+
+    return schemaMongo.define('Patient', {
+      id : { type: Number, index: true },
+      token : { type: String },
+      email : { type: String, limit: 150 },
+      name: { type: String, limit: 50 }
+    });
+  };
+
+})(exports);
--- tests/server/models-spec.js
+++ tests/server/models-spec.js
@@ -35,5 +35,41 @@ describe('DemS models', function() {
     });
 
   });
+  
+describe('Patient model', function() {
+    var patientModel, 
+        settings = require('../../dems.conf.json');
+
+    beforeEach(function() {
+      patientModel = require('../../models/patient.js').init(settings.db.mongoTest);
+    });
+
+    afterEach(function(done) {
+      patientModel.destroyAll(function(thing) {
+        patientModel.count(function(err, count) {
+          expect(count).toBe(0);
+          done();
+        });
+      });
+    });
+
+    it('should create new patient', function(done) {
+
+      var newPatient = {
+        id : 11,
+        token : 'fat',
+        name : 'James',
+        email : 'james@email.com'
+      };
+
+      patientModel.create(newPatient, function(err, patient) {
+        expect(err).toBe(null);
+        expect(patient.id).toBe(11);
+        done();
+      });
+
+    });
+
+  });
 
 });
