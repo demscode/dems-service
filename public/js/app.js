9f934ef2e66f466577179a64d2a1423a503e5d51
frankieferr <frankstar_frankilicious@hotmail.com>
2014-08-30 12:55:25 +1000

    Made views depedant on controllers which pass in data to the views

--- public/js/app.js
+++ public/js/app.js
@@ -5,14 +5,6 @@
 (function() {
 	var app = angular.module('DemS', ['ngRoute']);
 
-	app.config(['$routeProvider', function($routeProvider) {
-			$routeProvider.when('/patients', {templateUrl: '../views/partials/patients.html'})
-			$routeProvider.when('/logs', {templateUrl: '../views/partials/logs.html'})
-			$routeProvider.when('/account_details', {templateUrl: '../views/partials/account-details.html'})
-			$routeProvider.otherwise({redirectTo: '/patients'})
-	}]);
-
-
 	app.directive("frontPage", function() {
 	  return {
 	    restrict: 'E',
@@ -26,7 +18,7 @@
   	};
 	}]);
 
-	app.controller("CarerDetailsController", ['$http', function($http){
+	app.controller("AccountDetailsController", ['$http', function($http){
 		var ctrl = this;
 		$http.get("/api/currentuser").success(function(data) {
 			ctrl.carer = data;
@@ -45,4 +37,47 @@
       $(ctrl.form).submit();
     };
   } ] );
+
+  app.controller("PatientsController", ['$http', function($http){
+		this.patients = [
+			{
+				id: "123",
+				first_name: "Frankie",
+				last_name: "Ferraioli",
+				contact_number: "0477 881 223",
+				last_location: "Bedroom"
+			},
+			{
+				id: "124",
+				first_name: "Harley",
+				last_name: "Jonelynas",
+				contact_number: "0423 123 433",
+				last_location: "Living Room"
+			},
+		]
+  } ] );
+
+  app.controller("LogsController", ['$http', function($http){
+		this.logs = [
+			{
+				id: "1",
+				log_info1: "hello",
+				log_info2: "sup",
+				log_info3: "YOO",
+			},
+			{
+				id: "1",
+				log_info1: "HAHAH",
+				log_info2: "Oh Yeah",
+				log_info3: "LOLOLOL",
+			},
+		]
+  } ] );
+
+  app.config(['$routeProvider', function($routeProvider) {
+			$routeProvider.when('/patients', {templateUrl: '../views/partials/patients.html', controller:'PatientsController', controllerAs:'patientsCtrl'})
+			$routeProvider.when('/logs', {templateUrl: '../views/partials/logs.html', controller:'LogsController', controllerAs:'logsCtrl'})
+			$routeProvider.when('/account_details', {templateUrl: '../views/partials/account-details.html', controller:'AccountDetailsController', controllerAs:'accountCtrl'})
+			$routeProvider.otherwise({redirectTo: '/patients'})
+	}]);
 })();
