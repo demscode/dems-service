e17a440e0b7f3355fb8ec43aca72b218dbce657f
Harley Jonelynas <hdjonelynas@gmail.com>
2014-08-31 15:47:30 +1000

    Modified maps-example & controller.

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,29 +1,54 @@
 /**
  * DemS.maps
  *
+ *
  */
-(function() {
-	var app = angular.module('DemS.maps', ['google-maps']);
 
-  app.controller('MapsController', function($scope) {
-    $scope.map = {
-      center: {
-        latitude: -27.4679,
-        longitude: 153.0278
-      },
-      zoom: 14
+
+
+(function() {
+	var app = angular.module('DemS.maps', []);
+
+  app.controller('MapsController', function($http) {
+    var maps = this;
+    var path = [];
+    var map, polygon, marker;
+
+    this.mapSettings =  {
+      div: '#map',
+      lat: -27.4679,
+      lng: 153.0278
     };
 
-    $scope.marker = {
-      id: 0,
-      title: "QUT - Gardens Point",
-      coords: {
-        latitude: -27.477112,
-        longitude: 153.028015
-      },
-      options: {
-        draggable: false
-      }
+    this.CreateMap = function(settings) {
+      maps.map = GMaps(settings);
+    };
+
+    this.CreatePolygon = function(paths) {
+      maps.polygon = maps.map.drawPolygon({
+        paths: maps.path,
+        editable: true,
+        strokeColor: '#66b266',
+        strokeOpacity: 1,
+        strokeWeight: 3,
+        fillColor: '#b2d8b2',
+        fillOpacity: 0.6
+      });
+    };
+
+    this.CreateMarker = function(position, outofbounds) {
+      maps.marker = maps.map.addMarker({
+        lat: position.latitude,
+        lng: position.longitude,
+        draggable: false,
+        fences: [maps.polygon],
+        outside: outofbounds
+      });
+    };
+
+    this.init = function(patientid) {
+
+
     };
 
   });
