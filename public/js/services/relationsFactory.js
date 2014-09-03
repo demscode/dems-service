000593059ab632f56a63ff8884eb6737f7ba6cf8
Kristian Matzen <kristian_motzen@hotmail.com>
2014-09-03 20:57:25 +1000

    Updated relationsFactory to use resource

--- public/js/services/relationsFactory.js
+++ public/js/services/relationsFactory.js
@@ -1,21 +1,24 @@
-angular.module('DemS').factory('relations', ['$apiFactory', function($apiFactory){
+angular.module('DemS').factory('relationsFactory', ['$resource', function($resource){
 
 	var relationsFactory = {};
 
 	relationsFactory.addPatientToCarer = function(patientId, carerId){
 
-		var url = '/carer/:'+carerId+'/relationship';
-		var parameters = {link: true, patiendId : patientId};
-		
-		$apiFactory.put(url,parameters);
+		var url = '/carer/:carerId';
+
+     var carerResource = $resource(url, {carerId:'@carerId'});
+     carerResource.get({carerId:carerId}, function(carer) {
+       carer.patients.put(patientId);
+       carer.$save();
+     });
 	};
 
-	relationsFactory.removePatientFromCarer = function(patientId, carerId){
+	// relationsFactory.removePatientFromCarer = function(patientId, carerId){
 		
-		var url = '/carer/:'+carerId+'/relationship';
-		var parameters = {link: false, patientId : patientId};
+	// 	var url = '/carer/:'+carerId+'/relationship';
+	// 	var parameters = {link: false, patientId : patientId};
 		
-		$apiFactory.put(url,parameters);
+	// 	$apiFactory.put(url,parameters);
 	};
 
 	return relationsFactory;
