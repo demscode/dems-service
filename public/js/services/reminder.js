0729d73eef937846118b25997d1033aac0fee4a9
Kristian Matzen <kristian_motzen@hotmail.com>
2014-09-13 15:36:23 +1000

    Updated service to fit API

--- public/js/services/reminder.js
+++ public/js/services/reminder.js
@@ -1,5 +1,8 @@
 (function () {
     angular.module('DemS').factory("Reminder", function($resource) {
-      return $resource('');
+      return $resource('/api/patient/:id/reminder', {}, {
+        update: {method:'PUT', url:'/api/patient/:id/reminder/:reminderId'},
+        delete: {method:'DELETE', url:'/api/patient/:id/reminder/:reminderId'}
+      }););
     });
 })();
