079096fbdef491260048990a1267e3d66b6bae55
Oliver <olivergiess93@gmail.com>
2014-10-12 17:11:02 +1000

    Implemented switch statement in reminders controller to change the color of a string, which changes the events color.

--- public/js/controllers/reminders.js
+++ public/js/controllers/reminders.js
@@ -103,6 +103,29 @@
 
     self.getEventsFromReminders = function () {
       var reminderEvents = [];
+
+
+      /*
+         * The default turns the events purple for
+         * debugging purposes.
+      */
+      var statusColor = "pink";
+      switch($scope.reminders[i].acknowledgement){
+          case "none":
+          statusColor = "orange";
+          break;
+        case "OK":
+          statusColor = "green";
+          break;
+        /*
+         * The default turns the events purple for
+         * debugging purposes.
+         */
+        default:
+          statusColor = "purple";
+          break;
+      };
+
       for (var i = 0, length = $scope.reminders.length; i < length; i++) {
         var reminderEvent = {
           id: $scope.reminders[i].id,
@@ -110,6 +133,7 @@
           start: new Date($scope.reminders[i].time),
           end: new Date($scope.reminders[i].time + 60 * 60000),
           durationEditable: false,
+          color: statusColor,
         };
         reminderEvents.push(reminderEvent);
       }
